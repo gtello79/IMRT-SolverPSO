e00bc445dbe26f0c743f7ca8c3e5a9c5bf8f183c
Eclif <felipe.jorquera1997@gmail.com>
2019-04-11 16:55:36 -0300

    Arreglando la caga

--- IMRTsolver-masterPSO/tester.sh
+++ IMRTsolver-masterPSO/tester.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CONT=0
 while [	$CONT -lt 5 ]; do
-	./PSO --max_iter 2 --size 5 |grep "Best solution found" >> resultados.txt
+	./PSO --max_iter 100 --size 100 --iner 0.957893216 --c1 0.856789321 --c2 0.356789321 |grep "Best solution found" >> resultados.txt
 	let CONT=CONT+1
 done
