cecd87c34a255701f351df0e0afc14891fdc726a
gtello79 <gonzalotello79@gmail.com>
2019-06-14 19:14:23 +0000

    new graphies

--- IMRTsolver-masterPSO/plotConvergency.py
+++ IMRTsolver-masterPSO/plotConvergency.py
@@ -15,11 +15,17 @@ if os.path.exists(archivo):
         nuevo = linea.strip().split(" ")
         if(nuevo[0] == "Best"):
             if(nuevo[1] == "Global"):
-                par = (int(nuevo[-1]),float(nuevo[4]))
-                best_values.add(par)
-    for i in best_values:
-      x.append(i[0])
-      y.append(i[1]) 
+                #par = (int(nuevo[-1]),float(nuevo[4]))
+                NewX = int(nuevo[-1])
+                NewY = float(nuevo[4])
+                if(not(NewX in x)):
+                  x.append(int(nuevo[-1]))
+                  y.append(float(nuevo[4]))
+    #for i in best_values:
+    #  x.append(i[0])
+    #  y.append(i[1]) 
+    print(x)
+    print(y)
     plt.plot(x,y,'o-')
     plt.grid()
     plt.xlim(0)
@@ -29,4 +35,3 @@ if os.path.exists(archivo):
     lectura.close()     
 else: 
     print("No existe el archivo")
-print(best_values)
