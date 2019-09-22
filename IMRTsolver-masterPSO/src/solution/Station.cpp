fea24a7524ab9582df662595ed1f042e30b2934c
gtello79 <gonzalotello79@gmail.com>
2019-09-21 23:03:40 -0300

    cambios necesarios para obtener valores en experimentos

--- IMRTsolver-masterPSO/src/solution/Station.cpp
+++ IMRTsolver-masterPSO/src/solution/Station.cpp
@@ -710,10 +710,10 @@ namespace imrt {
         if (activeRange.first<0) continue;
         Veloc_Aperture[move][k].first = w*Veloc_Aperture[move][k].first + c1*r1*(A[move][k].first - Bpm[move][k].first) + c2*r2*(A[move][k].first - Bgm[move][k].first);
         Veloc_Aperture[move][k].second = w*Veloc_Aperture[move][k].second + c1*r1*(A[move][k].second - Bpm[move][k].second) + c2*r2*(A[move][k].second - Bgm[move][k].second);
-        if(Veloc_Aperture[move][k].first < -1) Veloc_Aperture[move][k].first = -1;
-        if(Veloc_Aperture[move][k].first > 1) Veloc_Aperture[move][k].first = 1;
-        if(Veloc_Aperture[move][k].second < -1) Veloc_Aperture[move][k].second = -1;
-        if(Veloc_Aperture[move][k].second > 1) Veloc_Aperture[move][k].second = 1;
+        //if(Veloc_Aperture[move][k].first < -1) Veloc_Aperture[move][k].first = -1;
+        //if(Veloc_Aperture[move][k].first > 1) Veloc_Aperture[move][k].first = 1;
+        //if(Veloc_Aperture[move][k].second < -1) Veloc_Aperture[move][k].second = -1;
+        //if(Veloc_Aperture[move][k].second > 1) Veloc_Aperture[move][k].second = 1;
       }
     }
     velocity_intensity(BestG, BestP, w , c1 , c2);
@@ -778,11 +778,11 @@ namespace imrt {
     double r2 = ((double)rand()/(RAND_MAX));
     vector<double> Bgm = BestG.getApertureIntensity();
     vector<double> Bpm = BestP.getApertureIntensity();
-    w = 0.001;
+   // w = 0.001;
     for(int a = 0; a < max_apertures ; a++){
       veloc_intensity[a] = w*veloc_intensity[a]+r1*c1*(intensity[a]-BestG.intensity[a]) + r2*c2*(intensity[a]-BestP.intensity[a]);
-      if(veloc_intensity[a] < -1) veloc_intensity[a] = -1;
-      if(veloc_intensity[a] > 1) veloc_intensity[a] = 1;
+      //if(veloc_intensity[a] < -1) veloc_intensity[a] = -1;
+      //if(veloc_intensity[a] > 1) veloc_intensity[a] = 1;
       cout << veloc_intensity[a] << " " << intensity[a] <<" "<< BestG.intensity[a] << " " <<BestP.intensity[a] << endl;
     }
   }
